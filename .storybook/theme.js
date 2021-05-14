38c56a8aba2d4e9434c88c7724690396c257f165
Alande Amorim <alande.amorim@gmail.com>
2021-05-14 10:57:36 -0300

    initial commit

---
+++ .storybook/theme.js
@@ -0,0 +1,20 @@
+import {create} from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+
+  colorSecondary: '#7159c1',
+
+  appBg: '#0b0a0d',
+  appContentBg: '#16161a',
+  appBorderColor: '#7159cs1',
+
+  textColor: '#fff',
+
+  barTextColor: '#E6E6E6',
+  barSelectedColor: '#fff',
+  barBg: '#1b1a1f',
+
+  brandTitle: 'Liberson',
+  brandhUrl: 'https://liberson.org/'
+});
